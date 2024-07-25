942a67e3d18054689b6bb05b2ba54ebc379e5343
ashley-aa511 <okeyoashley@gmail.com>
2024-07-25 12:50:19 +0000

    Implements getter and setter

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,46 +1,71 @@
-class HolbertonCourse {
-    constructor(name, length, students) {
-        this.name = name;
-        this.length = length;
-        this.students = students;
-    }
+/**
+ * Represents a Holberton Course.
+ */
+export default class HolbertonCourse {
+  /**
+   * Creates a new @see {@link HolbertonCourse}.
+   *
+   * @param {String} name - The name of the course.
+   * @param {Number} length - How long the course is (in months).
+   * @param {String[]} students - The names of students in the course.
+   */
+  constructor(name, length, students) {
+    this.name = name;
+    this.length = length;
+    this.students = students;
+  }
 
-    // Getter for name
-    get name() {
-        return this._name;
-    }
+  /**
+   * Gets the name of this course.
+   */
+  get name() {
+    return this._name;
+  }
 
-    // Setter for name with type checking
-    set name(value) {
-        if (typeof value !== 'string') {
-            throw new TypeError('Name must be a string');
-        }
-        this._name = value;
+  /**
+   * Sets the name of this course.
+   */
+  set name(value) {
+    if (typeof value !== 'string') {
+      throw new TypeError('Name must be a string');
     }
+    this._name = value;
+  }
 
-    // Getter for length
-    get length() {
-        return this._length;
-    }
+  /**
+   * Gets the length of this course (in months).
+   */
+  get length() {
+    return this._length;
+  }
 
-    // Setter for length with type checking
-    set length(value) {
-        if (typeof value !== 'number') {
-            throw new TypeError('Length must be a number');
-        }
-        this._length = value;
+  /**
+   * Sets the length of this course (in months).
+   */
+  set length(value) {
+    if (typeof value !== 'number') {
+      throw new TypeError('Length must be a number');
     }
+    this._length = value;
+  }
 
-    // Getter for students
-    get students() {
-        return this._students;
-    }
+  /**
+   * Gets the names of students in this course.
+   */
+  get students() {
+    return this._students;
+  }
 
-    // Setter for students with type checking
-    set students(value) {
-        if (!Array.isArray(value) || !value.every(student => typeof student === 'string')) {
-            throw new TypeError('Students must be an array of strings');
-        }
-        this._students = value;
+  /**
+   * Sets the names of students in this course.
+   */
+  set students(value) {
+    if (!(value instanceof Array)) {
+      throw new TypeError('Students must be an array of strings');
     }
+    if (!value.every((student) => typeof student === 'string')) {
+      throw new TypeError('Students must be an array of strings');
+    }
+    this._students = value;
+  }
 }
