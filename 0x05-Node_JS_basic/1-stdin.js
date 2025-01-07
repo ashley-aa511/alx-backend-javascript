5732de8adeebfc63a46f35878261fa38faca53ac
ashley-a511 <okeyoashley@gmail.com>
2025-01-07 19:37:47 +0000

    Process stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,27 +1,14 @@
-#!/usr/bin/node
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-// Import the readline module to handle user input
-const readline = require('readline');
+process.stdin.setEncoding('utf8');
 
-// Create an interface for input and output
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
+process.stdin.on('readable', () => {
+  const chunk = process.stdin.read();
+  if (chunk !== null) {
+    process.stdout.write(`Your name is: ${chunk}`);
+  }
 });
 
-// Display the welcome message and prompt for the user's name
-console.log('Welcome to Holberton School, what is your name?');
-
-// Wait for the user to input their name
-rl.question('', (name) => {
-  // Display the user's input
-  console.log(`Your name is: ${name}`);
-  
-  // Close the readline interface and display the closing message
-  rl.close();
-});
-
-// Event listener for when the readline interface is closed
-rl.on('close', () => {
-  console.log('This important software is now closing');
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
 });
